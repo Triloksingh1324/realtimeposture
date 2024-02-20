de052e898992378a8d52ad0cc6b2b40880f565c9
Triloksingh2804 <122295623+Triloksingh2804@users.noreply.github.com>
2024-02-20 18:07:59 +0530

    classification done

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from video_stream import VideoStream
+
+app = Flask(__name__)
+
+# Initialize the VideoStream object
+video_stream = VideoStream()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        choice = request.form['choice']
+        if choice == '1':
+            video_stream.set_classifier('pushup')
+        elif choice == '2':
+            video_stream.set_classifier('bicep')
+        elif choice == '3':
+            video_stream.set_classifier('plank')
+        elif choice == '4':
+            video_stream.set_classifier('Tree')
+        elif choice == '5':
+            video_stream.set_classifier('TPose')
+        elif choice == '6':
+            video_stream.set_classifier('WarriorPose')
+
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return video_stream.stream()
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5007)
---
+++ bicep_classifier.py
@@ -0,0 +1,36 @@
+import cv2
+import math
+import numpy as np
+from time import time
+from angle_calculator import AngleCalculator
+import mediapipe as mp
+
+class BicepClassifier:
+    def __init__(self):
+        self.prev_state = None
+        self.bicep_count = 0
+
+    def classify(self, landmarks, prev_state, output_image):
+        # Your classification logic here
+
+        left_elbow_angle = AngleCalculator().calculate_angle(landmarks[mp.solutions.pose.PoseLandmark.LEFT_SHOULDER.value],
+                                            landmarks[mp.solutions.pose.PoseLandmark.LEFT_ELBOW.value],
+                                            landmarks[mp.solutions.pose.PoseLandmark.LEFT_WRIST.value])
+
+        right_elbow_angle = AngleCalculator().calculate_angle(landmarks[mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                            landmarks[mp.solutions.pose.PoseLandmark.RIGHT_ELBOW.value],
+                                            landmarks[mp.solutions.pose.PoseLandmark.RIGHT_WRIST.value])
+
+        label = ''
+        if left_elbow_angle > 140 and left_elbow_angle < 170 or right_elbow_angle > 140 and right_elbow_angle < 170:            
+            if prev_state != 'down':
+                label = 'down'
+                prev_state = 'down'
+        elif left_elbow_angle > 50 and left_elbow_angle < 70 or right_elbow_angle > 50 and right_elbow_angle < 70:
+            if prev_state == 'down':
+                self.bicep_count += 1
+                prev_state = 'up'
+                label = 'up'
+        cv2.putText(output_image, f'Bicep: {self.bicep_count}', (10, 60), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)      
+
+        return output_image,label, prev_state 
