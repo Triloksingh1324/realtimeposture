dcad6bd118273cd70fb421d4247bf8cb491acc9a
Triloksingh2804 <122295623+Triloksingh2804@users.noreply.github.com>
2024-02-25 21:39:32 +0530

    rechanged

--- pose_detector.py
+++ pose_detector.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 class PoseDetector:
     def __init__(self):
         self.mp_pose = mp.solutions.pose
-        self.pose = self.mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.4, model_complexity=2)
+        self.pose = self.mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.7, model_complexity=2)
         self.mp_drawing = mp.solutions.drawing_utils
 
     def detect_pose(self, image):
